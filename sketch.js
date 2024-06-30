69d5084a24c58f298e4dfa582d7fc8784aebf238
mars028 <marianalimadamaceno@gmail.com>
2024-06-29 21:33:43 -0300

    alterando o arquivo html

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 function setup() {
-    createCanvas(600, 600);
-     background("black");
-  }
+  createCanvas(600, 600);
+   background("black");
+}
+
+
+function draw() {
   
+  stroke ("purple");
+  fill ("pink");
+ 
   
-  function draw() {
-    
-    stroke ("purple");
-    fill ("pink");
-   
-    
-    // console.log(mouseIsPressed);
-    
-    if (mouseIsPressed){
-      rect(mouseX, mouseY, 20, 35);
-    }
+  // console.log(mouseIsPressed);
+  
+  if (mouseIsPressed){
+    rect(mouseX, mouseY, 20, 35);
   }
+}
